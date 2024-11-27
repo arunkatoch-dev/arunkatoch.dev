38faef4adeece7312a92179047f5b4d539732083
Arun Katoch <106273391+arunkatoch-dev@users.noreply.github.com>
2024-11-28 00:07:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import vercel from 'vite-plugin-vercel';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),vercel()],
   server: {
     host: true,
+    port: process.env.PORT,
   },
   define: {
     "process.env": {},
